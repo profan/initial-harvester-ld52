6e87cf1edd032a342a6f9421b805fdadacf1193e
profan <robinhubner@gmail.com>
2023-01-09 13:33:41 +0000

    adjust push builds script

--- push_builds.sh
+++ push_builds.sh
@@ -2,45 +2,44 @@
 
 # build dir loonix
 # GODOT=godot
-# BUILD_DIR=/home/profan/godot-builds/memento-mori
+# BUILD_DIR=/home/profan/godot-builds/initial-harvester
 
 # build dir windows
 GODOT=/c/Personal/Tools/Godot/Godot_v3.5.1-stable_win64.exe
-BUILD_DIR=/c/Personal/Builds/thresher
+BUILD_DIR=/c/Personal/Builds/initial-harvester
 VERSION="1.0.0"
 
 # debug builds used to be the safer option, but we might as well hope release is fine? :P
 USE_DEBUG_BUILDS=false
 
 # set if we want to actually push to butler
-PUSH_TO_BUTLER=true
+PUSH_TO_BUTLER=false
 
 # make the DIRS
-mkdir -p $BUILD_DIR/th_win64/
-mkdir -p $BUILD_DIR/th_lin64/
-mkdir -p $BUILD_DIR/th_osx64/
-mkdir -p $BUILD_DIR/th_android/
+mkdir -p $BUILD_DIR/ih_win64/
+mkdir -p $BUILD_DIR/ih_lin64/
+mkdir -p $BUILD_DIR/ih_osx64/
+mkdir -p $BUILD_DIR/ih_web/
 
 # build THE FUCKER
 if $USE_DEBUG_BUILDS
 then
-	$GODOT --no-window --export-debug "Windows Desktop" $BUILD_DIR/th_win64/mm.exe
-	$GODOT --no-window --export-debug "Linux/X11" $BUILD_DIR/th_lin64/mm
-	$GODOT --no-window --export-debug "Mac OSX" $BUILD_DIR/th_osx64/th.zip
-	$GODOT --no-window --export-debug Android $BUILD_DIR/th_android/th.apk
+	$GODOT --no-window --export-debug "Windows Desktop" $BUILD_DIR/ih_win64/ih.exe
+	$GODOT --no-window --export-debug "Linux/X11" $BUILD_DIR/ih_lin64/ih
+	# $GODOT --no-window --export-debug "Mac OSX" $BUILD_DIR/ih_osx64/ih.zip
+	$GODOT --no-window --export-debug "HTML5" $BUILD_DIR/ih_web/index.html
 else
-	$GODOT --no-window --export "Windows Desktop" $BUILD_DIR/th_win64/th.exe
-	$GODOT --no-window --export "Linux/X11" $BUILD_DIR/th_lin64/th
-	$GODOT --no-window --export "Mac OSX" $BUILD_DIR/th_osx64/th.zip
-	$GODOT --no-window --export Android $BUILD_DIR/th_android/th.apk
+	$GODOT --no-window --export "Windows Desktop" $BUILD_DIR/ih_win64/ih.exe
+	$GODOT --no-window --export "Linux/X11" $BUILD_DIR/ih_lin64/ih
+	# $GODOT --no-window --export "Mac OSX" $BUILD_DIR/ih_osx64/ih.zip
+	$GODOT --no-window --export "HTML5" $BUILD_DIR/ih_web/index.html
 fi
 
 # push THE FUCKER, if it's time
-
 if $PUSH_TO_BUTLER
 then
-	butler push $BUILD_DIR/mm_win64 profan/thresher:win64-release --userversion $VERSION
-	butler push $BUILD_DIR/mm_lin64 profan/thresher:lin64-release --userversion $VERSION
-	butler push $BUILD_DIR/mm_osx64 profan/thresher:osx-release --userversion $VERSION
-	butler push $BUILD_DIR/mm_android profan/thresher:android-release --userversion $VERSION
+	butler push $BUILD_DIR/ih_win64 profan/initial-harvester:win64-release --userversion $VERSION
+	butler push $BUILD_DIR/ih_lin64 profan/initial-harvester:lin64-release --userversion $VERSION
+	# butler push $BUILD_DIR/ih_osx64 profan/initial-harvester:osx-release --userversion $VERSION
+	butler push $BUILD_DIR/ih_web profan/initial-harvester:web-release --userversion $VERSION
 fi
