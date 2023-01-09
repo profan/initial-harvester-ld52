da9a3b79293a9ac0746331c9786602f677ee24c8
profan <robinhubner@gmail.com>
2023-01-09 01:20:07 +0000

    rock: add particles on destruction

--- rock.gd
+++ rock.gd
@@ -2,10 +2,14 @@ extends StaticBody2D
 class_name Rock
 
 onready var shape = get_node("shape")
+onready var sprite = get_node("sprite")
+onready var particles = get_node("particles")
+
+onready var time_to_die = particles.lifetime
 
 const STARTING_HEALTH: float = 10.0
-
 var health: float = STARTING_HEALTH
+var death_timer: float = 0.0
 
 func _ready():
 	call_deferred("_turn_current_tile_to_dirt")
@@ -20,10 +24,14 @@ func _turn_current_tile_to_dirt():
 	tilemap.update_bitmask_area(current_tile_position)
 
 func _physics_process(delta):
-	if health <= 0.0:
+	death_timer -= delta
+	if health <= 0.0 and death_timer <= 0.0:
 		queue_free()
 
 func on_collided_with() -> float:
 	health = 0.0
 	shape.disabled = true
+	sprite.visible = false
+	particles.emitting = true
+	death_timer = time_to_die
 	return 10.0
